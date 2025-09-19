3ca9af7d7cd72fcc82cb8969d889edeb4ec2022d
Deiber <deiber601@gmail.com>
2025-09-18 20:14:19 -0400

    fc

---
+++ docker_nodejs_foo/src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/',      (req, res) => res.send('Hola root!'));
+app.get('/ping',  (req, res) => res.send('pong'));
+app.get('/echo', (req, res) => res.send('Simple Express multi-stage API'));
+app.get('/pong', (req, res) => res.send('ping'));
+
+app.listen(port, () => {
+  console.log(`Servidor en http://localhost:${port}`);
+});
