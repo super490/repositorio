94803bf5c69fef3a27f1d48fbd7f78914b80fe3e
Deiber <deiber601@gmail.com>
2025-09-20 17:19:13 -0400

    add /bash

--- docker_nodejs_foo/src/app.js
+++ docker_nodejs_foo/src/app.js
@@ -6,6 +6,7 @@ app.get('/',      (req, res) => res.send('Hola root!'));
 app.get('/ping',  (req, res) => res.send('pong'));
 app.get('/echo', (req, res) => res.send('Simple Express multi-stage API'));
 app.get('/pong', (req, res) => res.send('ping'));
+app.get('/bash', (req, res) => res.send('Es un interprete de comando'));
 
 app.listen(port, () => {
   console.log(`Servidor en http://localhost:${port}`);
